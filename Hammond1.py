33c5cf3e70ecfa3da9d38276469ce4019ce4d28f
chloehammond <clhammond821@gmail.com>
2022-05-02 12:36:37 -0500

    Restarted Hammond1.py

--- Hammond1.py
+++ Hammond1.py
@@ -1,88 +1,57 @@
-def print_menu():
-    print('\nWelcome to generic E-Commerce Store!')
+import math
+
+
+def text_metrics():
     print('Which of the following would you like to do?')
-    print('1. Add Item')
-    print('2. Update Item')
-    print('3. Delete Item')
-    print('4. Display Items by Name')
-    print('5. Display Items by Total')
-    print('6. View Cart')
-    print('7. Quit')
+    print('1. Check your GFI')
+    print('2. Check your RES')
+    print('3. Check your SMOG')
+    print('4. Check your ARI')
+    print('5. Quit')
 
 
-def add_item():
-    name = input('Enter the name of the item you want to add: ')
-    price = float(input('Enter the price of the item: '))
-    quantity = int(input('Enter the quantity of item to be purchased: '))
-    items.append(name)
-    prices.append(price)
-    quantities.append(quantity)
+def gfi():
+    sentences = int(input('Please enter the number of sentences in the sample: '))
+    words = int(input('Please enter the number of words in the sample: '))
+    big_words = int(input('Please enter the number of big words in the sample: '))
+    return 0.4 * ((words/sentences) + 100 * (big_words/words))
 
 
-def update_item():  # your lab work
-    name_update = input('Enter the name of the item to update: ')
-    price_update = float(input('Enter the updated price of the item: '))
-    quantity = int(input('Enter the updated quantity of item to be purchased: '))
+def res():
+    sentences = int(input('Please enter the number of sentences in the sample: '))
+    words = int(input('Please enter the number of words in the sample: '))
+    syllables = int(input('Please enter the number of syllables in the sample: '))
+    return 206.835 - 1.015 * (words/sentences) - 84.6 * (syllables/words)
 
 
-def delete_item():  # your lab work
-    pass
+def smog():
+    big_words = int(input('Please enter the number of big words in the sample: '))
+    a = (math.sqrt(big_words) / 10) * 10
+    return math.floor((a + 9) / 10 + 3)
 
 
-def sort_by_name():
-    items2 = items.copy()
-    prices2 = prices.copy()
-    quantities2 = quantities.copy()
-
-    for i in range(len(items)):
-        min_index = 0
-        for j in range(1, len(items2)):
-            if items2[j] < items2[min_index]:
-                min_index = j
-        items[i] = items2[min_index]
-        prices[i] = prices2[min_index]
-        quantities[i] = quantities2[min_index]
-        items2.pop(min_index)
-        prices2.pop(min_index)
-        quantities2.pop(min_index)
-    view()
+def ari():
+    sentences = int(input('Please enter the number of sentences in the sample: '))
+    words = int(input('Please enter the number of words in the sample: '))
+    characters = int(input('Please enter the numver of characters in the sample: '))
+    return 4.71 * (characters / words) + 0.5 * (words / sentences) - 21.43
 
 
-def sort_by_price():  # your lab work
-    pass
+if __name__ == '__main__':
+    option = 0
+    while True:
+        text_metrics()
+        option = int(input('Enter the number of your choice: '))
+        if option == 1:
+            gfi()
+        elif option == 2:
+            res()
+        elif option == 3:
+            smog()
+        elif option == 4:
+            ari()
+        elif option == 5:
+            print('Quitting')
+        else:
+            print('Option unavailable. Please choose an option displayed.')
 
-
-def view():
-    sum = 0
-    for i in range(len(items)):
-        sum += prices[i] * quantities[i]
-        #print({items[i] {prices[i]:.2f} X{quantities[i]} for {prices[i] * quantities[i]:.2f}})
-        print(f'Total: ${sum:.2f}')
-
-
-items = []  # store my products in a list because we want to order it later
-prices = []
-quantities = []
-
-option = 0
-while True:  # ending program controlled by option 7
-    print_menu()
-    option = int(input('Enter the number of your choice: '))
-
-    if option == 1:
-        add_item()
-    elif option == 2:
-        update_item()
-    elif option == 3:
-        delete_item()
-    elif option == 4:
-        sort_by_name()
-    elif option == 5:
-        sort_by_price()
-    elif option == 6:
-        view()
-    elif option == 7:
-        print('Quitting')
-        break
-    else:
-        print('Option unavailable. Please choose an option displayed.')
